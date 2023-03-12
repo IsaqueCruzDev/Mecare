c1442ea0b566bed2635fc07eabeeb1a9b067c0a9
Isaque de Oliveira cruz <oliveiraioc616@gmail.com>
2023-03-12 16:17:06 -0300

    site para instituição médica completo

---
+++ js/index.js
@@ -0,0 +1,12 @@
+// let menu = document.getElementById("menu-btn");
+// let navbar = document.getElementsByClassName("navbar");
+
+// menu.onclick = () => {
+//     menu.classList.toggle('fa-times');
+//     navbar.classList.toggle('active');
+// }
+
+// window.onscroll = () => {
+//     menu.classList.remove('fa-times');
+//     navbar.classList.remove('active');
+// }
